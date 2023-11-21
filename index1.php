ecd372db5a6ee655a8b126a09fc410fe99eeb1dc
gadanidev <nidhi.dhandhukiya@multidots.com>
2023-11-21 23:01:42 +0530

    fixhomescreenbutton

---
+++ index1.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="ś width=device-width, initial-scale=1.0">
+    <title>dm grocery </title>
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+</head>
+
+<body>
+    <div class="nav">
+
+        <div class="nav-menu">
+            <ul>
+                <li><a href="http://localhost/projectdev/index1.php" class="active">home</a></li>
+                <li><a href="devphp/shopping_cart.php">Products</a></li>
+                <li><a href="http://localhost/projectdev/aboutus.php">about us</a></li>
+                <li><a href="#">LOGIN</a>
+                    <div class="nav-menu-subit">
+                        <ul>
+                            <li><a href="devphp/adim.php">adim</a></li>
+                            <li><a href="devphp/login_pag.php">customer</a></li>
+
+                        </ul>
+                    </div>
+                </li>
+                <li><a href="devphp/registraton.php">signup</a></li>
+                <li><a href="#c">contact us</a></li>
+            </ul>
+        </div>
+        <div class="heading">
+            <h1 class="heading-h1">hi ,
+                <span class="heading-span">welcome
+                </span><span class="heading-span2 animated">to shop your products</span>
+            </h1>
+            <a href="http://localhost/projectdev/devphp/shopping_cart.php" class="btn btn-white animated" value="submit">ORDER NOW</a>
+        </div>
+
+    </div>
+    <main>
+        <section class="section-about">
+            <div class="u-center-text u-mrgbottom-8">
+                <h2 class="section-about-heading">
+                    related about pricing of the website
+                </h2>
+
+                <div class="row">
+                    <div class="col-1-of-2 ">
+                        <h3 class="section-about-heading1 u-mrgbottom-small">
+                            you will get the best price here
+                        </h3>
+                        <p class="section-about-p-pragraph u-mrgbottom-small">
+                            in your website u will get same prices which is at store
+                            and you will get some new offer here online if u purchase here online
+                        </p>
+
+                        <a href="#" class="btn-text">shop now &rarr; </a>
+                    </div>
+                    <div class="col-1-of-2 ">
+                        <div class="compostion">
+                            <img src="img/clipPath1.jpeg" alt="photo1  " class="compostion__photo compostion__photo--p1">
+                            <img src="img/clipPath2.jpeg" alt="photo2" class="compostion__photo compostion__photo--p2">
+                            <img src="img/clipPath3.jpeg" alt="photo3" class="compostion__photo compostion__photo--p3">
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="c" class="section-contactus">
+            <h3 class="conhead">contactus</h3>
+            <div class="section-contactus-conform">
+
+                <form method="POST" action="../projectdev/devphp/index_contactus.php"><br />
+                    <label for="contact">full name</label><br />
+                    <input type="text" name="username" id="uid" required>
+                    <label for="email"> email</label><br />
+                    <input type="email" name="emailadd" id="conemail" required><br />
+                    <label for="phone">phone number</label><br />
+                    <input type="tel" name="phone_number" id="phone_id" required><br />
+                    <label for="sub">subject</label><br />
+                    <input type="text" name="subject" id="sub_id" required><br />
+                    <label for="message">message</label><br />
+                    <input type="text" name="message" id="mes_id" cols='4' rows='5' required><br />
+                    <input type="submit" value="send message" name="submit" class="btn-con"><br />
+                </form>
+            </div>
+
+
+    </main>
+</body>
+
+</html>
